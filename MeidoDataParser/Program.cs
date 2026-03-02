ddd41a18ca1f51961e61a6306a5b0c19e17ea824
Zsolt Zitting <zsolt@yello.ooo>
2026-03-02 08:44:47 -0800

    What was he thinking (me 5 years ago)

--- MeidoDataParser/Program.cs
+++ MeidoDataParser/Program.cs
@@ -3,151 +3,155 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace MeidoDataParser
 {
+    public class GameInfo
+    {
+        public string game { get; set; }
+        
+        public List<FileInfo> files { get; set; }
+    }
+    
+    public class FileInfo
+    {
+        public string filepath { get; set; }
+        
+        public List<UpdateFile> updates { get; set; }
+    }
+    
     public class UpdateFile
     {
-        public string fileName;
-        public string updateFileName;
-        public long size;
-        public string hash;
-        public int version;
-    }
-
-    public class UpdatePack
-    {
-        public string name;
-        public string company;
-        public string appName;
-        public string appExe;
-        public string category;
-        public string registry;
-        public string language;
-        public List<UpdateFile> files;
+        // file size of the update file
+        public long size { get; set; }
+        
+        // file hash of update file
+        public string hash { get; set; }
+        
+        // the internal version number
+        public int version { get; set; }
+        
+        // the name of the archive where this file was found
+        public string source { get; set; }
     }
 
     public class ProgramSettings
     {
-        public string filter;
-        public bool outputMini;
-        public bool outputFull;
-        public string searchDir;
-        public bool noDLC;
-        public bool showUnknownKeys;
+        public string filter { get; set; }
+        public bool outputMini { get; set; }
+        public bool outputFull { get; set; }
+        public string searchDir { get; set; }
+        public bool noDLC { get; set; }
+        public bool showUnknownKeys { get; set; }
     }
 
-    class Program
+    public class Program
     {
-        static List<UpdatePack> packs = new List<UpdatePack>();
+        private static List<GameInfo> _games = new();
 
-        static ProgramSettings settings = null;
+        private static ProgramSettings _settings;
 
-        static void Main(string[] args)
+        private static void CheckMissing()
         {
-            try { settings = JsonConvert.DeserializeObject<ProgramSettings>(File.ReadAllText("config.json")); }
+            List<string> missingLst = new();
+
+            foreach (string iniPath in Directory.EnumerateFiles(_settings.searchDir, "*.ini",
+                         SearchOption.AllDirectories))
+            {
+                if(Directory.GetFiles(Path.GetDirectoryName(iniPath), "*.lst").Length == 0)
+                    missingLst.Add(Path.GetFileName(Path.GetDirectoryName(iniPath)));
+            }
+
+            if (missingLst.Count > 0)
+            {
+                Console.WriteLine("Some updates are missing file lists!\nLists Missing:\n  - " + string.Join("\n  - ", missingLst));
+                Console.WriteLine("\nSaved to missing.txt. The program will now exit.");
+                File.WriteAllLines(Path.Combine(_settings.searchDir, "missing.txt"), missingLst);
+                Environment.Exit(1);
+            }
+            else
+            {
+                File.Delete(Path.Combine(_settings.searchDir, "missing.txt"));
+            }
+            
+        }
+
+        private static void Main(string[] args)
+        {
+            try { _settings = JsonSerializer.Deserialize<ProgramSettings>(File.ReadAllText("config.json")); }
             catch(FileNotFoundException)
             {
                 Console.WriteLine("HALT: No config.");
                 Environment.Exit(1);
             }
+
+            CheckMissing();
             
-            ReadData(settings.filter);
+            ReadData(_settings.filter);
 
-            var jsonSettings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
-            string json = JsonConvert.SerializeObject(packs, Formatting.Indented, jsonSettings);
-            string jsonMin = JsonConvert.SerializeObject(packs, Formatting.None, jsonSettings);
+            var jsonSettings = new JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            };
+            var jsonSettingsIndent = new JsonSerializerOptions(jsonSettings) { WriteIndented = true };
+            string json = JsonSerializer.Serialize(_games, jsonSettingsIndent);
+            string jsonMin = JsonSerializer.Serialize(_games, jsonSettings);
+            
+            File.WriteAllText(Path.Combine(_settings.searchDir, @$"kiss{(string.IsNullOrEmpty(_settings.filter)?"":$"-{_settings.filter}")}.json"), json, Encoding.UTF8);
+            File.WriteAllText(Path.Combine(_settings.searchDir, @$"kiss{(string.IsNullOrEmpty(_settings.filter)?"":$"-{_settings.filter}")}.min.json"), jsonMin, Encoding.UTF8);
 
-            //Console.WriteLine(jsonIgnoreNullValues);
-            File.WriteAllText(Path.Combine(settings.searchDir, @$"kiss{(string.IsNullOrEmpty(settings.filter)?"":$"-{settings.filter}")}.json"), json, Encoding.UTF8);
-            File.WriteAllText(Path.Combine(settings.searchDir, @$"kiss{(string.IsNullOrEmpty(settings.filter)?"":$"-{settings.filter}")}.min.json"), jsonMin, Encoding.UTF8);
-
-            Console.WriteLine($"Successfully parsed {packs.Count} update packs!");
+            Console.WriteLine($"Successfully parsed info for {_games.Count} games!");
         }
 
-        static void ReadData(string filter)
+        private static void ReadData(string filter)
         {
-            foreach (string file in Directory.EnumerateFiles(settings.searchDir, "*.lst", SearchOption.AllDirectories))
+            foreach (string gamePath in Directory.EnumerateDirectories(_settings.searchDir, "*",
+                         SearchOption.TopDirectoryOnly))
             {
-                if (!string.IsNullOrEmpty(filter) && !file.Contains(filter)) continue;
-                if (settings.noDLC && file.Contains(Path.Combine(settings.searchDir, "DLC"))) continue;
-
-                var pack = new UpdatePack { name = Path.GetFileName(Path.GetDirectoryName(file)) };
-
-                ParseIni(pack, Directory.GetFiles(Path.GetDirectoryName(file), "*.ini")[0]);
-                ParseLst(pack, file);
-
-                packs.Add(pack);
-            }
-        }
-
-        static void ParseIni(UpdatePack pack, string filename)
-        {
-            var ini = new Ini(filename);
-            pack.company = ini.GetValue("Company", "UPDATER", null);
-            pack.appName = ini.GetValue("AppName", "UPDATER", null);
-            pack.appExe = ini.GetValue("AppExe", "UPDATER", null);
-            pack.registry = ini.GetValue("Registry1", "UPDATER", null);
-            pack.category = ini.GetValue("Category", "UPDATER", null);
-            pack.language = ini.GetValue("Language", "UPDATER", null);
-
-            if(settings.showUnknownKeys) // hides "slow" LINQ behind config option, for speed emphasis.
-            {
-                string[] known = {
-                    "Company",
-                    "AppName",
-                    "AppExe",
-                    "Registry1",
-                    "Category",
-                    "Language",
-                    // unimportant
-                    "ShortcutName",
-                    "ShortcutExe",
-                    "ShortcutParam",
-                    "VerExe",
-                    "ReqVer",
-                    "Company2",
-                    "Registry2",
-                    "RejectVerRangeMin",
-                    "RejectVerRangeMax",
-                    "RejectVerMsg",
-                    "CheckFile",
-                    "CheckFileCRC32"
-                };
-
-                foreach (var key in ini.GetKeys("UPDATER").Except(known))
+                var game = new GameInfo {game = Path.GetFileName(gamePath)};
+                
+                game.files = new List<FileInfo>();
+                
+                foreach (string file in Directory.EnumerateFiles(gamePath, "*.lst", SearchOption.AllDirectories))
                 {
-                    Console.WriteLine($"[INFO] Unknown key \"{key}\" with value \"{ini.GetValue(key, "UPDATER", null)}\" in {pack.name}");
+                    if (!string.IsNullOrEmpty(filter) && !file.Contains(filter)) continue;
+                    if (_settings.noDLC && file.Contains(Path.Combine(gamePath, "_DLC"))) continue;
+
+                    var source = Path.GetFileName(Path.GetDirectoryName(file));
+                
+                    ParseLst(game.files, file, source);
                 }
+                
+                _games.Add(game);
             }
-            
-            
         }
 
-        static void ParseLst(UpdatePack pack, string filename)
+        private static void ParseLst(List<FileInfo> fileinfos, string filename, string source)
         {
             var files = File.ReadAllLines(filename);
-
-            var fileList = new List<UpdateFile>();
 
             foreach (var fileDef in files)
             {
                 if(string.IsNullOrEmpty(fileDef)) continue;
                 var file = new UpdateFile();
                 string[] args = fileDef.Split(',');
-                file.fileName = args[2];
-                file.updateFileName = (args[0] == "share" && args[1] != "0") ? args[1] : null;
+                
+                var filepath = args[2];
+                var newInfo = false;
+
+                var fileInfo = fileinfos.Find(x => x.filepath == filepath) ??
+                               new FileInfo {filepath = filepath, updates = [] };
+                
                 file.size = long.Parse(args[3]);
                 file.hash = args[4];
                 file.version = int.Parse(args[5]);
-
-                fileList.Add(file);
+                file.source = source;
+                
+                if (fileInfo.updates.All(x => x.hash != file.hash || x.size != file.size)) fileInfo.updates.Add(file);
+                if (!fileinfos.Contains(fileInfo)) fileinfos.Add(fileInfo);
             }
-
-            if (fileList.Count < 1) throw new InvalidDataException("No files found in update.lst, this shouldn't happen.");
-
-            pack.files = fileList;
         }
     }
 }
